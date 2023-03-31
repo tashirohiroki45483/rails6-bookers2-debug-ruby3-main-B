a18d6d10d105d847b29f0c7a5b616bd0ebdfa6bd
EC2 Default User <ec2-user@ip-172-31-13-161.ap-northeast-1.compute.internal>
2023-03-31 14:45:26 +0000

    7b

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,12 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @books = @user.books
     @book = Book.new
+    @posts_today_count = posts_today_count(@user)
+    @posts_yesterday_count = posts_yesterday_count(@user)
+    @posts_difference = posts_difference(@user)
+    @posts_this_week_count = posts_this_week_count(@user)
+    @posts_last_week_count = posts_last_week_count(@user)
+    @posts_week_difference = posts_week_difference(@user)
   end
 
   def index
@@ -34,6 +40,42 @@ class UsersController < ApplicationController
     unless @user == current_user
       redirect_to user_path(current_user)
     end
+  end
+
+  def posts_today_count(user)
+    user.books.where('created_at >= ?', Time.zone.now.beginning_of_day).count
+  end
+
+  def posts_yesterday_count(user)
+    user.books.where(created_at: 1.day.ago.beginning_of_day..1.day.ago.end_of_day).count
+  end
+
+  def posts_difference(user)
+    today_count = posts_today_count(user)
+    yesterday_count = posts_yesterday_count(user)
+    if yesterday_count.zero?
+      return 0 if today_count.zero?
+      return 100
+    end
+    ((today_count.to_f / yesterday_count) * 100).round(2)
+  end
+
+  def posts_this_week_count(user)
+    user.books.where(created_at: Time.zone.now.beginning_of_week..Time.zone.now.end_of_week).count
+  end
+
+  def posts_last_week_count(user)
+    user.books.where(created_at: 1.week.ago.beginning_of_week..1.week.ago.end_of_week).count
+  end
+
+  def posts_week_difference(user)
+    this_week_count = posts_this_week_count(user)
+    last_week_count = posts_last_week_count(user)
+    if last_week_count.zero?
+      return 0 if this_week_count.zero?
+      return 100
+    end
+    ((this_week_count.to_f / last_week_count) * 100).round(2)
   end
 
 end
